49397b5f62b7399fe5bd2332927928ad3204c2fe
Lisa Pereira <lisapereira3172003@gmail.com>
2022-02-03 14:29:36 +0530

    demo

---
+++ chatterbot/My Chatbot/Pairs.py
@@ -0,0 +1,12 @@
+response_pairs=[
+    [('What (.*) website (.*)'),["Fr. Conceicao Rodrigues College Website: http://www.frcrce.ac.in/index.php "]],
+    [('Which department|departments|courses (.*)'),['Available Courses: \n Computer Engineering \n Electronics and Computer Science \n Information Technology \n Science and Humanities \n AI & Data Science']],
+    [('hello|hola|holla|hi|hey (.*)'),['Hey, How can I help you?']],
+    [('Thanks|Thank You|Thank You (.*)'),["Youre welcome! Do come back if you have more questions!"]],
+    [('Where (.*) College'),['The College is at Bandra']],
+    [('(.*) Contact|reach (.*)'),['You can reach the College Office with +91 (22) 6711 4000/+91 (22) 6711 4100/ crce@frcrce.ac.in']],
+    [('(.*) Notice (.*)'),['Go To College Notices by clicking the link: http://www.frcrce.ac.in/index.php/students/crce-notices']],
+    [('quit'),['Hope I Helped! Do come back if you have more questions.']],
+    [('ok|okay|k|kk (.*)'),['Do you want to ask anything else?']],
+    [('(.*)'),["Sorry!I can't understand what you said there"]]
+]
